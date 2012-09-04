6c095ef8a706d7d1b4c03db2d74c0f20654ddff4
Sergey Kashirin <sergey.kashirin@cyberhull.com>
2012-09-04 23:27:25 +0400

    all

---
+++ app/code/local/Cyberhull/Helloworld/controllers/GoController.php
@@ -0,0 +1,6 @@
+<?php
+class Cyberhull_Helloworld_GoController extends Mage_Core_Controller_Front_Action {
+	public function toAction() {
+		echo 'go to!';
+	}
+}
---
+++ app/code/local/Cyberhull/Helloworld/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+class Cyberhull_Helloworld_IndexController extends Mage_Core_Controller_Front_Action {
+	public function indexAction() {
+		echo 'Hello Index!';
+	}
+	
+	public function myAction() {
+		echo 'my my my!';
+	}
+}
